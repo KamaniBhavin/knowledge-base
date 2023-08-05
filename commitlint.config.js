9ec95db939e2180ca008d9e2fde705801f3ce338
Bhavin Kamani <13havinkamani@gmail.com>
2023-08-05 13:54:09 +0530

    feat(*): Let the fun begin!

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,30 @@
-module.exports = { extends: ["@commitlint/config-conventional"] };
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    formatter: "@commitlint/format",
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "build",
+                "chore",
+                "ci",
+                "docs",
+                "feat",
+                "fix",
+                "perf",
+                "refactor",
+                "revert",
+                "style",
+                "test",
+            ],
+        ],
+        "type-case": [2, "always", "lower-case"],
+        "type-empty": [2, "never"],
+        "scope-empty": [2, "never"],
+        "scope-case": [2, "always", "lower-case"],
+        "subject-case": [2, "always", "sentence-case"],
+        "subject-empty": [2, "never"],
+        "header-max-length": [2, "always", 72],
+    },
+};
